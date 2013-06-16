0bdbe98388ce3af4e23c8e061b9345e8c970bc7c
root <root@uberfl.us>
2013-06-16 05:58:57 +0000

    fixed feed

--- poab/views/feed.py
+++ poab/views/feed.py
@@ -54,33 +54,46 @@ def rss_view(request):
     for log in logs:
         twitter = False
         guid = None
-        print log.infomarker_id
-        q = DBSession.query(Trackpoint).filter(Trackpoint.id==log.infomarker_id)
-        infomarker=q.one()
-        # ###query for last trackpoint
-        q = DBSession.query(Trackpoint).filter(and_(Trackpoint.track_id==infomarker.track_id,Trackpoint.id==infomarker.id)).order_by(asc(Trackpoint.timestamp))
-        lasttrkpt=q.first()
-        q = DBSession.query(Track).filter(Track.id==infomarker.track_id)
-        if q.count() == 1:
-            track=q.one()
+        print log.infomarker
+        #q = DBSession.query(Trackpoint).filter(Trackpoint.id==log.infomarker)
+        #infomarker=q.one()
+        ## ###query for last trackpoint
+        #q = DBSession.query(Trackpoint).filter(and_(Trackpoint.track_id==infomarker.track_id,Trackpoint.id==infomarker.id)).order_by(asc(Trackpoint.timestamp))
+        #lasttrkpt=q.first()
+        #q = DBSession.query(Track).filter(Track.id==infomarker.track_id)
+        #if q.count() == 1:
+        #    track=q.one()
+        #    # ###calculate duration from track-info
+        #    total_mins = track.timespan.seconds / 60
+        #    mins = total_mins % 60
+        #    hours = total_mins / 60
+        #    timespan = str(hours)+'h '+str(mins)+'min'
+        #    rounded_distance=str(track.distance.quantize(Decimal("0.01"), ROUND_HALF_UP))+'km'
+        if len(log.tracks) > 0:
             # ###calculate duration from track-info
-            total_mins = track.timespan.seconds / 60
-            mins = total_mins % 60
-            hours = total_mins / 60
+            total_seconds = 0
+            total_distance = Decimal(0)
+            for track in log.tracks:
+                total_seconds = total_seconds + track.timespan.seconds
+                total_distance = total_distance + Decimal(track.distance)
+            total_minutes = total_seconds / 60
+            mins = total_minutes % 60 #full minutes left after division by 60
+            hours = total_minutes / 60
             timespan = str(hours)+'h '+str(mins)+'min'
-            rounded_distance=str(track.distance.quantize(Decimal("0.01"), ROUND_HALF_UP))+'km'
+            rounded_distance=str(total_distance.quantize(Decimal("0.01"), ROUND_HALF_UP))+'km'
+            print timespan, rounded_distance
         else:
             rounded_distance=None
             timespan=None
         # ###query for timezone and calculate localtime
-        q = DBSession.query(Timezone).filter(Timezone.id==infomarker.timezone_id)
         try:
+            q = DBSession.query(Timezone).filter(Timezone.id==8)
             timezone = q.one()
             localtime=log.created+timezone.utcoffset
         except:
             localtime=log.created
         # ###query for country and continent
-        q = DBSession.query(Country).filter(Country.iso_numcode==infomarker.country_id)
+        q = DBSession.query(Country).filter(Country.iso_numcode==792)
         country=q.one()
         q = DBSession.query(Continent).filter(Continent.id==country.continent_id)
         continent=q.one()
@@ -88,8 +101,8 @@ def rss_view(request):
         if p.search(log.topic):
             guid=p.search(log.topic).group("guid")
             twitter=True
-        log_bla=log.content
-        imgidtags=re.findall('\[imgid[0-9]*\]',log_bla)
+        log_content_display=log.content
+        imgidtags=re.findall('\[imgid[0-9]*\]',log_content_display)
         print imgidtags
         print log.id
         for imgidtag in imgidtags:
@@ -110,13 +123,13 @@ def rss_view(request):
             <a href="http://www.flickr.com/peletiah/%s" target="_blank">www.flickr.com</a>
     </div></div><span class="imagedescription">%s</span></div>''' % (image_large,imageinfo.flickrtitle,imageinfo.flickrfarm,imageinfo.flickrserver,imageinfo.flickrphotoid,imageinfo.flickrsecret,imageinfo.flickrtitle,imageinfo.flickrphotoid,imageinfo.flickrdescription)
 
-                log_bla=log_bla.replace(imgidtag,inlineimage)
+                log_content_display=log_content_display.replace(imgidtag,inlineimage)
         urlfinder = re.compile('^(http:\/\/\S+)')
         urlfinder2 = re.compile('\s(http:\/\/\S+)')
         def urlify_markdown(value):
             value = urlfinder.sub(r'<\1>', value)
             return urlfinder2.sub(r' <\1>', value)
-        log_bla=markdown.markdown(urlify_markdown(log_bla))
+        log_content_display=markdown.markdown(urlify_markdown(log_content_display))
         class Logdetails(object):
                 def __init__(self, topic, twitter, guid, localtime, content, rounded_distance, timezone, timespan, country, continent, lasttrkpt, infomarker, log):
                     self.topic=topic
@@ -135,10 +148,10 @@ def rss_view(request):
                         self.timespan=None
                     self.country=country.iso_countryname
                     self.continent=continent.name
-                    self.location=lasttrkpt.location
-                    self.infomarkerid=infomarker.id
+                    self.location=lasttrkpt.location_ref[0].name
+                    self.infomarkerid=log.trackpoint_log_ref.id
                     self.id=log.id
-        logdetails = Logdetails(log.topic, twitter, guid, localtime, log_bla, rounded_distance, timezone, timespan, country, continent, lasttrkpt, infomarker, log)
+        logdetails = Logdetails(log.topic, twitter, guid, localtime, log_content_display, rounded_distance, timezone, timespan, country, continent, log.trackpoint_log_ref, log.trackpoint_log_ref, log)
         logdetaillist.append(logdetails)
     
     request.response.content_type = "application/atom+xml"
